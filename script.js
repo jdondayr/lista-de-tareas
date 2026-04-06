e11f46ca6c97ffde28205b91a189f96b4fa141aa
Juan <jdondayr@gmail.com>
2026-04-06 16:03:35 +0200

    Añado botones para cambiar las tareas de lista, cambio tamaños de fuentes, introduzco las tareas junto al botón en un div nuevo que se crea al añadir la tarea.

--- script.js
+++ script.js
@@ -10,21 +10,43 @@ let listaCompletadas = document.querySelector("#listaCompletadas");
 let inputTareas = document.querySelector("#inputTareas");
 
 // Acciones
-botonAñadirTarea.addEventListener("click", () => {
-    let tareaNueva = document.createElement("li");
-    tareaNueva.textContent = inputTareas.value;
 
+// Click en el botón de Añadir tarea
+botonAñadirTarea.addEventListener("click", () => {
+    // Si el input está vacío
     if (inputTareas.value === "") return;
 
-    tareaNueva.addEventListener("click", () => {
-        if (tareaNueva.parentElement === listaPorHacer) listaCompletadas.appendChild(tareaNueva);
-        else if (tareaNueva.parentElement === listaCompletadas) listaPorHacer.appendChild(tareaNueva);
-    })
-    
+    // Crea div tarea nueva con un párrafo con la tarea y un botón para cambiarla de lista
+    let tareaNueva = document.createElement("div");
+    tareaNueva.classList.add("divTareas");
+    let tareaParrafo = document.createElement("p");
+    tareaParrafo.textContent = inputTareas.value;
+    let botonTarea = document.createElement("button");
+    botonTarea.classList.add("botonTarea");
+    botonTarea.textContent = "Marcar como completada"
+
+    // Introducir párrafo y botón en el div tarea
+    tareaNueva.appendChild(tareaParrafo);
+    tareaNueva.appendChild(botonTarea);
+
+    // Click en la tarea para cambiarla a la otra lista
+    botonTarea.addEventListener("click", () => {
+        if (tareaNueva.parentElement === listaPorHacer){
+            botonTarea.textContent = "Marcar como no completada"
+            listaCompletadas.appendChild(tareaNueva);
+        }
+        else if (tareaNueva.parentElement === listaCompletadas){
+            botonTarea.textContent = "Marcar como completada";
+            listaPorHacer.appendChild(tareaNueva);
+        }
+        })
+
+    // Introducir la tarea en la lista Por Hacer
     listaPorHacer.appendChild(tareaNueva);
     inputTareas.value = "";
 })
 
+// Click en botón de eliminar para eliminar las tareas de las listas
 botonBorrarTareas.addEventListener("click", () => {
     listaPorHacer.textContent = "";
     listaCompletadas.textContent = "";
